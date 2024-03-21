b7879a85393657c0ec85bee1ff9229e8dee6e594
zoeurk <zoeurk@gmail.com>
2024-03-21 12:49:09 +0100

    update

--- Tor.VMs/sbin/VMs.sh
+++ Tor.VMs/sbin/VMs.sh
@@ -1,3 +1,7 @@
 #!/bin/dash
+until -e /tmp/connected/connected
+do
+	sleep 1
+done
 . /opt/VMs/vm-tor-relay.src
 . /opt/VMs/vm-tor-exit.src
--- Tor.VMs/sbin/online.sh
+++ Tor.VMs/sbin/online.sh
@@ -1,5 +1,8 @@
 #!/bin/dash
+test -d /tmp/connected && rm -rf /tmp/connected
+mkdir /tmp/connected
 until ping -i 1 -c 1 -w 1 -W 1 -q $1 > /dev/null;
 do
 	sleep 1;
 done
+touch /tmp/connected/connected.ok
